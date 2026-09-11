6e69f173f91863a5e08ca0e02aa46672b1d98506
danczlife-arch <dan.czlife@gmail.com>
2026-09-11 17:21:43 +0200

    commit

---
+++ profile_generator.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+height = input("How tall are you?: ")
+hobby = input("What is your hobby?: ")
+
+print("Hello "+name)
+print("You are "+age+" years old")
+print("You are "+height+" metres tall")
+print("Hobby "+hobby+" is great" )
