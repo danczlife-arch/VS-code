41f316a50c70ebd0e4d5f50d32674592baeccb53
danczlife-arch <dan.czlife@gmail.com>
2026-09-16 18:23:21 +0200

    mini_quiz.py

---
+++ mini_quiz.py
@@ -0,0 +1,20 @@
+question = "What is 2 + 2?"
+answer = input(question + " ")
+if answer == "4":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+question = "What is the capital of Czech Republic?"
+answer = input(question + " ")
+if answer == "Prague":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+question = "What is 11 - 7?"
+answer = input(question + " ")
+if answer == "4":
+    print("Correct!")
+else:
+    print("Incorrect!")
